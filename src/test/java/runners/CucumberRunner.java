c31eb7e80b54d25f70be050ae74f4e3df9833d4a
daveteam21 <91298376+daveteam21@users.noreply.github.com>
2023-08-19 16:32:37 -0400

    Committing Files

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = { "src/test/resources/features" }, glue = {
+@CucumberOptions(features = { "src/test/resources/features/ClearHistory2.feature" }, glue = {
 		"stepdefinition" }, tags = "", plugin = { "html:target/cucumber-reports.html" })
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
